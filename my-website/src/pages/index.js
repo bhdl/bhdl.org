5053b46d35fcf0f69fa6883fbae99f51dc5a29b2
Hebi Li <lihebi.com@gmail.com>
2021-04-20 22:47:52 +0800

    fill index page

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -13,17 +13,28 @@ import {
   Center,
   Heading,
   Text,
+  Alert,
+  AlertIcon,
+  Image,
 } from "@chakra-ui/react"
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <Box
+      // className={clsx("hero hero--primary", styles.heroBanner)}
+      className={styles.heroBanner}
+      bgGradient="linear(to-l, #74ebd5 0%, #ACB6E5 50%)"
+    >
       <div className="container">
         {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
         {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
-        <Heading mb={10}>{siteConfig.title}</Heading>
-        <Text>{siteConfig.tagline}</Text>
+        <Heading as="h1" mb={10} size="4xl">
+          {/* {siteConfig.title} */}
+          Board HDL
+        </Heading>
+        <Text fontSize="xl">{siteConfig.tagline}</Text>
+
         <Center>
           <Flex mt={10}>
             <Box mr={10}>
@@ -37,9 +48,9 @@ function HomepageHeader() {
             <Box>
               <Link
                 className="button button--secondary button--lg"
-                to="/docs/intro"
+                to="/examples"
               >
-                Read the Docs
+                Browse Examples
               </Link>
             </Box>
           </Flex>
@@ -54,7 +65,49 @@ function HomepageHeader() {
           </Link>
         </div> */}
       </div>
-    </header>
+    </Box>
+  )
+}
+
+function Features() {
+  return (
+    <Box>
+      <Flex mb={10} mt={10}>
+        <Flex bgColor="gray.100" p={5} boxShadow="xl" direction="column" mx={5}>
+          <Heading size="md" mb={2} fontWeight="lighter">
+            PCB as a Program
+          </Heading>
+          <Text>
+            Ex cillum proident minim quis consequat et irure exercitation anim
+            sunt amet voluptate ut est. Reprehenderit reprehenderit esse duis
+            nisi est commodo ipsum veniam ex aute. Commodo amet consequat
+            officia qui cillum enim fugiat eu non.
+          </Text>
+        </Flex>
+        <Flex bgColor="gray.100" p={5} boxShadow="xl" direction="column">
+          <Heading size="md" mb={2} fontWeight="lighter">
+            Lucid, Expressive, Embedded
+          </Heading>
+          <Text>
+            Officia labore culpa excepteur culpa laboris deserunt. Minim cillum
+            do laborum est nisi laborum. Magna irure irure proident dolor
+            proident fugiat elit ad incididunt amet. Commodo quis tempor laborum
+            aliqua cillum duis eiusmod enim duis in.
+          </Text>
+        </Flex>
+        <Flex bgColor="gray.100" p={5} boxShadow="xl" direction="column" mx={5}>
+          <Heading size="md" mb={2} fontWeight="lighter">
+            Auto-Placement &amp; Routing
+          </Heading>
+          <Text>
+            In ut tempor consequat duis magna laboris eu nulla exercitation
+            eiusmod ullamco. Irure nostrud et culpa amet occaecat. Reprehenderit
+            adipisicing minim aliquip commodo dolore laboris occaecat magna
+            excepteur. Amet ipsum sit do do non adipisicing esse
+          </Text>
+        </Flex>
+      </Flex>
+    </Box>
   )
 }
 
@@ -66,10 +119,39 @@ export default function Home() {
         title={`HOME - ${siteConfig.title}`}
         description="A Programming Language and System for making PCBs"
       >
-        <HomepageHeader />
-        <main>
-          <HomepageFeatures />
-        </main>
+        <Box bgColor="var(--ifm-hero-background-color)">
+          <HomepageHeader />
+          <Center my={5}>
+            <Alert maxW="xl" status="warning">
+              <AlertIcon />
+              This site is under construction, please check back later (soon!)
+            </Alert>
+          </Center>
+
+          <main>
+            <Box>
+              <Center>
+                <Image
+                  src="../../static/img/images/workflow.png"
+                  alt="alt"
+                  w="lg"
+                />
+              </Center>
+            </Box>
+            <Features></Features>
+            <Box>
+              <Center>
+                <Image
+                  src="../../static/img/images/jupyter.png"
+                  alt="jupyter"
+                  w={0.9}
+                  mb={10}
+                />
+              </Center>
+            </Box>
+            {/* <HomepageFeatures /> */}
+          </main>
+        </Box>
       </Layout>
     </ChakraProvider>
   )
